722c7da36d784c7f1c5768dd31348c75a179e1dc
yanntrividic <yann.trividic@hotmail.fr>
2021-02-27 13:06:40 +0100

    Methods added to PreProcessing and Utils

--- src/cv/PreProcessing.java
+++ src/cv/PreProcessing.java
@@ -1,5 +1,26 @@
 package cv;
 
-public class PreProcessing {
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
+public class PreProcessing {
+	
+	/**
+	 * Creates a GrayScale Mat object out of a RGB Mat object using OpenCV
+	 * @param rgb A Mat object that contains the image to process
+	 * @return a new Mat object with the CV_8UC1 type
+	 */
+	public static Mat rgbToGrayScale(Mat rgb) {
+		Mat gs = new Mat();
+		Imgproc.cvtColor(rgb, gs, Imgproc.COLOR_RGB2GRAY); // to grayscale
+		return gs ;
+	}
+	
+	public static Mat equalizeGrayMat(Mat gray) {
+		if(gray.type() != CvType.CV_8UC1) throw new IllegalArgumentException("The image must be CV_8UC1") ;
+		Mat equalized = new Mat();
+		// TODO: continue this method with Imgproc.equalizeHist()
+		return equalized ;
+	}
 }
---
+++ src/cv/Utils.java
@@ -0,0 +1,27 @@
+package cv;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+
+public class Utils {
+	
+	/**
+	 * If a Point object isn't within the boundaries of the Mat object, the returned Point object has coordinates
+	 * within the ranges of the Mat.
+	 * @param mat The reference image
+	 * @param p The point to clone and modify
+	 * @return A new Point object within the boundaries of the image
+	 */
+	public static Point getPointWithinBoundaries(Mat mat, Point p) {
+		Point vp = p.clone() ;
+		int width = mat.width() ;
+		int height = mat.height() ;
+		
+		if(p.x > width) vp.x = width ;
+		if(p.x < 0) vp.x = 0 ;
+		if(p.y > height) vp.y = height ;
+		if(p.y < 0) vp.y = 0 ;
+		
+		return vp ;		
+	}
+}
