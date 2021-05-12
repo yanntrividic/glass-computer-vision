5b2606a4192c94ef4836aafbbe07c55096c6404a
yanntrividic <yann.trividic@hotmail.fr>
2021-05-12 15:43:14 +0200

    Renommage de GestionC4

--- src/cv/EllipseFinder.java
+++ src/cv/EllipseFinder.java
@@ -1,8 +1,10 @@
 package cv;
 
 import java.awt.Color;
+//import java.awt.Point;
 import java.util.ArrayList;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.RotatedRect;
@@ -13,14 +15,14 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 public class EllipseFinder {
-
+	
 	/**
 	 * The function that finds the ellipse with the best score. This function calls the methods drawEllipse, getFirstPixel,getLeftPixel,getRightPixel
 	 * @param img a picture with a glass on it
 	 * @param mask the mask to isolate the glass from the rest of the image
 	 * @return
 	 */
-	public static ArrayList<Point> getEllipse(Mat img,Mat mask){
+	public static Mat getEllipse(Mat img,Mat mask){  //ArrayList<Point> getEllipse(Mat img,Mat mask
 		//merge the mask and the image
 		//Core.multiply(img, mask, img);
 		//show(img,"avant");
@@ -32,56 +34,66 @@ public class EllipseFinder {
 		//show(img,"apres fusion");
 		//show(img,"ca marche");
 		//cv.Segmentation.simpleBinarization(mask, 1, false);
-
-
+		
+		
 		//merge the mask and the image
 		//img=cv.Segmentation.simpleBinarization(mask, 1, false);
 		//Core.multiply(img, mask, img);
 		//show(img,"test1");
-
-
+		
+		
 		//we resize the image to limit the number of operations
 		//img=cv.PreProcessing.resizeSpecifiedWidth(img,500); //PARAMETER
-
-
+		
+		
 		//the starting position for the course
 		Point startPos=getFirstPixel(img);
-
+			
 		//startPos.x=startPos.x+(img.height()*10)/100;  //� verifier
 		Point endPos=new Point();
-
+		
 		//the end position, it corresponds to 2/3 of the image, this value is arbitrary and must be changed
 		endPos.y=(img.height()/100)*80; //� revoir
-
-
+		
+		
 		//the list of points of the ellipse with the best score 
 		ArrayList<Point> bestEllipse=new ArrayList<Point>();
 		//we create a first ellipse to be able to compare
 		bestEllipse=drawEllipse(startPos,getRightPixel(img,(int)startPos.x,(int)startPos.y),img,10);
-
+		
 		////run through the image, increasing the height by 5pixels as you go
 		for(int i=(int)startPos.y+5;i<endPos.y;i+=5) {   //y parameter
 			//get the left and right pixels at height i
 			Point left=getLeftPixel(img,i);
 			Point right=getRightPixel(img,(int)left.x,(int)left.y);
-
+			
 			////we create all possible ellipses at this height, z corresponds to its height, it starts at 10 and increases from 10 to 80
 			for(int z=10;z<80;z+=5) { //z parameter
-
+				
 				ArrayList<Point> tempEllipse=drawEllipse(left,right,img,z);   //drawEllipse(new Point(i,yS),new Point(i,yE),path);
 				//the current score is compared with the score of the ellipse 
 				if(getEllipseScore(imgComp, tempEllipse)>getEllipseScore(imgComp, bestEllipse)) {
 					bestEllipse=tempEllipse;
 				}
 			}
-
+			
 		}
 		//show the best Ellipse
-		testDrawn(bestEllipse,img);
-
-		return bestEllipse;
+		//testDrawn(bestEllipse,img);
+		return(testDrawn(bestEllipse,img));
+		//return bestEllipse;
 	}
-	
+	/**
+	 * allows you to load an image
+	 * @param path the path of the image 
+	 * @return the loaded image
+	 */
+	public static Mat loadPicture(String path) {
+		nu.pattern.OpenCV.loadLocally();
+		Imgcodecs imageCodecs =new Imgcodecs();
+		Mat matrix=imageCodecs.imread(path);
+		return matrix;
+	}
 	/**
 	 * Gets the leftmost and top pixel of the glass
 	 * @param img the image where the glass is
@@ -89,7 +101,7 @@ public class EllipseFinder {
 	 */
 	public static Point getFirstPixel(Mat img) {
 		Point p=new Point(0,0);
-
+		
 		for(int i=(img.width()/100)*10;(i<img.width())&&(p.x==0);i++) {                  //on commence pas � 0, mais � 10 % de la taille
 			for(int j=(img.height()/100)*30;j<img.height()&&p.x==0;j++) {                  //30%
 				if(img.get(j, i)[0]!=0) {
@@ -99,7 +111,6 @@ public class EllipseFinder {
 		}
 		return p;
 	}
-	
 	/**
 	 * 
 	 * @param img the image where the glass is
@@ -115,7 +126,6 @@ public class EllipseFinder {
 		}
 		return p;
 	}
-	
 	/**
 	 * 
 	 * @param img the image where the glass is
@@ -125,15 +135,14 @@ public class EllipseFinder {
 	 */
 	public static Point getRightPixel(Mat img,int start,int heigth) {
 		Point p=new Point(0,0);
-		for(int i=start;i<img.width()&&p.x==0;i++) {
-
+		for(int i=img.width()/2;i<img.width()&&p.x==0;i++) {//avant i=start
+			
 			if((img.get(heigth, i+1)[0]==0)||(i==(img.width()-2))) {
 				p=new Point(i,heigth);
 			}
 		}
 		return p;
 	}
-	
 	/*
 	public static Point getLastPixel(Mat img,int start) {
 		Point p=new Point(0,0);
@@ -146,8 +155,7 @@ public class EllipseFinder {
 		}
 		return p;
 	}
-	 */
-	
+	*/
 	/**
 	 * 
 	 * @param left The left point, the starting point of the ellipse
@@ -157,44 +165,46 @@ public class EllipseFinder {
 	 * @return a list of points corresponds to the plotted ellipse
 	 */
 	public static ArrayList<Point> drawEllipse(Point left,Point right,Mat src,int height) {
-
-		//Reading the source image in to a Mat object
-
-		//Reading the source image in to a Mat object
-		//Mat src = Imgcodecs.imread(path);
-		//Drawing an Ellipse
-		//src=resize(src);
-		Mat temp = new Mat();
-		temp= src.clone();//The image is cloned each time so that the ellipses are not made directly on the image, as they would then overlap
-
-		Size sz=new Size(right.x-left.x,height);  //the size of the future box
-
-		Point center=new Point((left.x+right.x)/2,left.y);//the centre of the ellipse 
-
-		RotatedRect box = new RotatedRect(center,sz,0);//the last parameter is the tilt angle, and MAY BE A PARAMETER 
-		Scalar color = new Scalar(255, 255, 255); //the colour of the curve, here it is white MAY BE A PARAMETER 
-		int thickness = 1;
-		Imgproc.ellipse (temp, box, color, thickness);  //the ellipse is drawn
-
-		//Imgproc.ellipse(src, new Point(150,150), new Size(260, 180), 25.0, 25.0, 25.0, color); test autres arguments
-
-		//HighGui.imshow("Drawing an ellipse"+right, temp);//displays the ellipsis, can be deleted
-		// HighGui.waitKey(10);//the value 10 allows ellipses to be displayed in a consistent manner
-
-		return getEllipseDraw(temp,Color.white,(int)right.y);
-
-	}
-
-	public static void testDrawn(ArrayList<Point>list,Mat img) {
+		
+	      //Reading the source image in to a Mat object
+	      
+	    //Reading the source image in to a Mat object
+	      //Mat src = Imgcodecs.imread(path);
+	      //Drawing an Ellipse
+	      //src=resize(src);
+		  Mat temp = new Mat();
+		  temp= src.clone();//The image is cloned each time so that the ellipses are not made directly on the image, as they would then overlap
+		 
+	      Size sz=new Size(right.x-left.x,height);  //the size of the future box
+	     
+	      Point center=new Point((left.x+right.x)/2,left.y);//the centre of the ellipse 
+	      
+	      RotatedRect box = new RotatedRect(center,sz,0);//the last parameter is the tilt angle, and MAY BE A PARAMETER 
+	      Scalar color = new Scalar(255, 255, 255); //the colour of the curve, here it is white MAY BE A PARAMETER 
+	      int thickness = 1;
+	      Imgproc.ellipse (temp, box, color, thickness);  //the ellipse is drawn
+	
+	      //Imgproc.ellipse(src, new Point(150,150), new Size(260, 180), 25.0, 25.0, 25.0, color); test autres arguments
+	      
+	      //HighGui.imshow("Drawing an ellipse"+right, temp);//displays the ellipsis, can be deleted
+	     // HighGui.waitKey(10);//the value 10 allows ellipses to be displayed in a consistent manner
+	      
+	   return getEllipseDraw(temp,Color.white,(int)right.y);
+	      
+	   }
+	
+	public static Mat testDrawn(ArrayList<Point>list,Mat img)
+	{
 		double[] col= {212.0, 43.0, 48.0};
 		for(int i=0;i<list.size();i++) {
-
+			
 			img.put((int)list.get(i).x,(int)list.get(i).y,col);
 		}
-
-		show(img,"testDrawn");
+		
+		//show(img,"testDrawn");
+		return img;
 	}
-
+	
 	/**
 	 * For testing purposes only
 	 * @param matrix
@@ -203,9 +213,13 @@ public class EllipseFinder {
 	public static void show(Mat matrix,String legend) {
 		HighGui.imshow(legend, matrix);
 		HighGui.waitKey(0);
-		//System.exit(0);	
+		//System.exit(0);
+		
 	}
+	
+	
 
+	
 	/**
 	  allows to obtain the ellipse drawn previously
 	 * @param img picture where the ellipse is
@@ -219,14 +233,13 @@ public class EllipseFinder {
 				double[]temp=img.get(i, j);
 				//for(int k=0;k<temp.length;k++) {
 				//System.out.println(temp[0]+" "+temp[1]+" "+temp[2]);
-				if(temp[0]==255) {//&&temp[1]==255&&temp[2]==255)
-					ellipse.add(new Point(i,j));
+					if(temp[0]==255) {//&&temp[1]==255&&temp[2]==255)
+						ellipse.add(new Point(i,j));
 				}
 			}
 		}
 		return ellipse;
 	}
-
 	/**
 	 * Calculates the score of an ellipse using Sagi Eppel's method, "Point by point evaluation of the curve environment"
 	 * @param img the image where the ellipse should be
@@ -244,7 +257,7 @@ public class EllipseFinder {
 		//Mat temp=new Mat();
 		//grayscale before calculating the score
 		//Imgproc.cvtColor(img, temp, Imgproc.COLOR_RGB2GRAY);
-
+		
 		for(int i=0;i<ellipse.size();i++) {
 			sommU+=img.get((int)ellipse.get(i).x-1, (int)ellipse.get(i).y)[0];    //image en noir et blanc 
 			if(img.get((int)ellipse.get(i).x-1, (int)ellipse.get(i).y)[0]>maxU)
@@ -262,9 +275,8 @@ public class EllipseFinder {
 		else
 			max=maxD;	
 		return((meanU-meanD)/max);
-
+		
 	}
-
 	/**
 	 * allows you to resize the image to avoid too many operations
 	 * @param img the image to resize
@@ -287,7 +299,7 @@ public class EllipseFinder {
 		}
 		return res;
 	}
-
+	
 	public static Mat fusionImgMask(Mat img,Mat mask) {
 		for(int i=0;i<mask.height();i++) {
 			for(int j=0;j<mask.width();j++) {
@@ -299,51 +311,36 @@ public class EllipseFinder {
 		}
 		return img;
 	}
-
-
-
-	// TODO: DELETE EVENTUALLY
-	
-	/**
-	
-	 * allows you to load an image
-	 * @param path the path of the image 
-	 * @return the loaded image
-	 */
-	public static Mat loadPicture(String path) {
-		nu.pattern.OpenCV.loadLocally();
-		Imgcodecs imageCodecs =new Imgcodecs();
-		Mat matrix=imageCodecs.imread(path);
-		return matrix;
-	}
+//not use
 	
 	public static void main(String[]args) {
 		//drawEllipse();
 		//drawEllipse(new Point(0,0),new Point(0,0));
-	
+		
 		//CONVERTIR L'IMAGE DES LE DEBUT
 		//System.exit(0);
 		Mat img=loadPicture("E:\\image\\image1.png");
-	
+		
 		//Imgproc.resize( img, img, sz );
-	
+		
 		Mat mask=loadPicture("E:\\image\\image2.png");
-	
+		
 		//Imgproc.cvtColor(img2, img2, Imgproc.COLOR_BGR2GRAY);
-	
+		
 		getEllipse(img, mask);
-	
+		
 		//System.out.println("point de dep"+ startPos);
-	
+		
 		//System.out.println("point de droite"+ droite);
 		//drawEllipse(startPos,droite,img);
-	
-	
-	
+		
+		
+		
 	
 		//drawEllipse(gauche,droite,path);
+		
 	
 	}
-
+	
 
 }
